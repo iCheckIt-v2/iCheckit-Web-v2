0eed2d93433bbcc3cbdfd52e9d841cec40563728
Miguel Aquino <jcaquino0945@gmail.com>
2021-08-12 10:32:39 +0800

    forgot pass and form validation custom

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA2JZxtbvpQWqxJDjw3HsUdgNXX9B29I40',
+    authDomain: 'icheckit-6a8bb.firebaseapp.com',
+    projectId: 'icheckit-6a8bb',
+    storageBucket: 'icheckit-6a8bb.appspot.com',
+    messagingSenderId: '160010861440',
+    appId: '1:160010861440:web:6770d29d102be5703f5f65',
+    measurementId: 'G-MZWH337QWV'
+  }
 };
