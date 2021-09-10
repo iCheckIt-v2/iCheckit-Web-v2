c44fafccc18de88a650fd70a956daa8b9b745bd0
Miguel Aquino <jcaquino0945@gmail.com>
2021-09-10 13:58:48 +0800

    recipient layout

--- src/app/dashboard-components/task/task.component.ts
+++ src/app/dashboard-components/task/task.component.ts
@@ -50,6 +50,7 @@ export class TaskComponent implements OnInit {
     ).subscribe((res) => {
       this.taskData = res;
       console.log(res);
+      console.log(res.recipients.length);
     })
   }
 
