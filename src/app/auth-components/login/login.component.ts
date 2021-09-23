9d9d11fa1c32d752b9770508c4a0a2b48bd0910c
Hans Nituda <hanspatrick.nituda.iics@ust.edu.ph>
2021-09-24 00:28:27 +0800

    initial changes

--- src/app/auth-components/login/login.component.ts
+++ src/app/auth-components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { AngularFireAuth  } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { ToastService } from 'src/app/services/toast.service';
 import { FormBuilder } from '@angular/forms';
@@ -9,15 +9,16 @@ import { Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  name!:string;
-  number!:string;
+  show: boolean = false;
+  name!: string;
+  number!: string;
   loginForm!: any;
   constructor(
     public auth: AuthService,
-    readonly fire: AngularFireAuth, 
+    readonly fire: AngularFireAuth,
     public router: Router,
     public toastService: ToastService,
     private fb: FormBuilder
@@ -25,7 +26,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required,Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
@@ -38,12 +39,17 @@ export class LoginComponent implements OnInit {
 
   public login() {
     if (this.loginForm.valid) {
-      this.auth.signIn(this.loginForm.controls['email'].value,this.loginForm.controls['password'].value)
-    }
-    else if (this.loginForm.invalid) {
-    this.loginForm.controls['email'].markAsTouched();
-    this.loginForm.controls['password'].markAsTouched();
-    this.toastService.publish('Please fill up all required fields properly','formError');
+      this.auth.signIn(
+        this.loginForm.controls['email'].value,
+        this.loginForm.controls['password'].value
+      );
+    } else if (this.loginForm.invalid) {
+      this.loginForm.controls['email'].markAsTouched();
+      this.loginForm.controls['password'].markAsTouched();
+      this.toastService.publish(
+        'Please fill up all required fields properly',
+        'formError'
+      );
     }
     /*
     if (this.email == '' && this.password == '') {
@@ -51,7 +57,9 @@ export class LoginComponent implements OnInit {
     if (this.email != '' && this.password != '') {
       this.auth.signIn(this.email,this.password)
     } */
-
   }
-  
+
+  password() {
+    this.show = !this.show;
+  }
 }
