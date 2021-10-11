d9b157a67859345e150e459932204f44a9273631
Hans Nituda <hanspatrick.nituda.iics@ust.edu.ph>
2021-10-11 23:26:18 +0800

    ui changes + student count in reports

--- src/app/dashboard-components/task-settings/task-settings.component.ts
+++ src/app/dashboard-components/task-settings/task-settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Params, ActivatedRoute } from '@angular/router';
-import { AngularFireAuth  } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from 'src/app/services/auth.service';
 import { TaskService } from 'src/app/services/task.service';
 import { switchMap } from 'rxjs/operators';
@@ -9,27 +9,33 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ToastService } from 'src/app/services/toast.service';
 // modal dialog import
 @Component({
-	selector: 'task-settings',
-	templateUrl: './task-settings.component.html',
-	styleUrls: ['./task-settings.component.css']
+  selector: 'task-settings',
+  templateUrl: './task-settings.component.html',
+  styleUrls: ['./task-settings.component.css'],
 })
-
 export class TaskSettingsComponent implements OnInit {
+  term!: string;
   dateToday = new Date();
-  userData:any;
+  userData: any;
   fsData: any;
-   id!: any;
+  id!: any;
   taskData: any;
-  taskStatus: any = ['Pending','Completed']
+  taskStatus: any = ['Pending', 'Completed'];
   // start of edit task import
   // taskScope: any = ['1ITA','1ITB','1ITC','1ITD','1ITE','1ITF','1ITH','2ITA','2ITB','2ITC','2ITD','2ITE','2ITF','3ITA','3ITB','3ITC','3ITD','3ITF','3ITG','3ITF','3ITG','4ITA','4ITB','4ITC','4ITD','4ITE','BS Information Technology', 'BS Information Systems', 'BS Computer Science'];
   taskScopeArray!: string[];
   // newTaskScope: any = ['1ITA','1ITB','1ITC','1ITD','1ITE','1ITF','1ITH','2ITA','2ITB','2ITC','2ITD','2ITE','2ITF','3ITA','3ITB','3ITC','3ITD','3ITF','3ITG','3ITF','3ITG','4ITA','4ITB','4ITC','4ITD','4ITE','BS Information Technology', 'BS Information Systems', 'BS Computer Science'];
   newTaskScopeArray!: string[];
-  addTaskForm!:any;
+  addTaskForm!: any;
   newTaskForm!: any;
-  taskRecipients: { uid: any; status: string; section: any; submissionLink: string; displayName: any; }[] = [];
-  userPushTokens: { pushToken: string; }[] = [];
+  taskRecipients: {
+    uid: any;
+    status: string;
+    section: any;
+    submissionLink: string;
+    displayName: any;
+  }[] = [];
+  userPushTokens: { pushToken: string }[] = [];
   addTaskModal!: boolean;
   addScopeModal!: boolean;
   editTaskScope!: any;
@@ -43,7 +49,7 @@ export class TaskSettingsComponent implements OnInit {
   // editTaskScope! : any;
   // end of edit task import
 
-	constructor(
+  constructor(
     public auth: AuthService,
     private route: ActivatedRoute,
     private router: Router,
@@ -51,89 +57,91 @@ export class TaskSettingsComponent implements OnInit {
     private taskService: TaskService,
     private fb: FormBuilder,
     public toastService: ToastService
+  ) {}
 
-  ) { }
-
-	ngOnInit(): void {
+  ngOnInit(): void {
+    var d = new Date();
+    var y = d.getFullYear();
+    var n = d.getMonth();
+    console.log(n);
+    console.log(y);
+    if (n >= 1 && n <= 6) {
+      console.log('January to June');
+      console.log('2nd Term SY ' + y + '-' + (y + 1));
+      this.term = '2nd Term SY ' + y + '-' + (y + 1);
+    } else if (n >= 8 && n <= 12) {
+      console.log('August to December');
+      console.log('1st Term SY ' + y + '-' + (y + 1));
+      this.term = '1st Term SY ' + y + '-' + (y + 1);
+    } else {
+      console.log('Summer Term' + y + '-' + (y + 1));
+      this.term = 'Summer Term' + y + '-' + (y + 1);
+    }
     this.updateTaskForm = this.fb.group({
-      title: ['',Validators.required],
-      description: ['',Validators.required],
-      deadline: ['',Validators.required],
-
-
-    })
-    this.fire.user.subscribe((user:any) => {
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      deadline: ['', Validators.required],
+    });
+    this.fire.user.subscribe((user: any) => {
       this.userData = user;
-      this.auth.getUserData(user?.uid).subscribe(res => {
+      this.auth.getUserData(user?.uid).subscribe((res) => {
         this.fsData = res;
-      })
-    })
-      this.route.params.pipe(
+      });
+    });
+    this.route.params
+      .pipe(
         switchMap((params: Params) => {
           console.log(params['id']);
-          return this.taskService.getTask(params['id'])
+          return this.taskService.getTask(params['id']);
         })
-      ).subscribe((res) => {
-        this.updateTaskForm.controls.title.setValue(res.title)
-        this.updateTaskForm.controls.description.setValue(res.description)
-        this.updateTaskForm.controls.deadline.setValue(res.deadline)
+      )
+      .subscribe((res) => {
+        this.updateTaskForm.controls.title.setValue(res.title);
+        this.updateTaskForm.controls.description.setValue(res.description);
+        this.updateTaskForm.controls.deadline.setValue(res.deadline);
         this.taskData = res;
         console.log(res);
-      })
+      });
 
-  this.taskScopeArray = [];
+    this.taskScopeArray = [];
+  }
 
-
-
-
-   }
-
-
-
-
-
-
-   public triggerDeleteTaskModal() {
+  public triggerDeleteTaskModal() {
     this.deleteTaskModal = !this.deleteTaskModal;
-
   }
 
   public triggerUpdateTask() {
-
     this.updateTaskForm.controls.title.setValue(this.taskData.title);
-    this.updateTaskForm.controls.description.setValue(this.taskData.description);
+    this.updateTaskForm.controls.description.setValue(
+      this.taskData.description
+    );
     this.updateTaskForm.controls.deadline.setValue(this.taskData.deadline);
-
-
   }
 
   public updateTask() {
-
     if (this.updateTaskForm.valid) {
-      this.taskService.updateTask(
-        this.taskData.taskId,
-        this.updateTaskForm.controls['title'].value,
-        this.updateTaskForm.controls['description'].value,
-        new Date (this.updateTaskForm.controls['deadline'].value),
-
-
-      ).then(() => this.triggerUpdateTask())
-      .finally(() => this.updateTaskForm.reset())
-    }
-    else if (this.updateTaskForm.invalid) {
+      this.taskService
+        .updateTask(
+          this.taskData.taskId,
+          this.updateTaskForm.controls['title'].value,
+          this.updateTaskForm.controls['description'].value,
+          new Date(this.updateTaskForm.controls['deadline'].value)
+        )
+        .then(() => this.triggerUpdateTask())
+        .finally(() => this.updateTaskForm.reset());
+    } else if (this.updateTaskForm.invalid) {
       this.updateTaskForm.controls['title'].markAsTouched();
       this.updateTaskForm.controls['description'].markAsTouched();
       this.updateTaskForm.controls['deadline'].markAsTouched();
 
-
-      this.toastService.publish("Please fillup all the requirements","updateError");
-
+      this.toastService.publish(
+        'Please fillup all the requirements',
+        'updateError'
+      );
     }
-
   }
 
-
-   changeTaskScope(e:any) {
+  changeTaskScope(e: any) {
     console.log(e.target.value);
     console.log(typeof e.target.value);
     // if (this.taskScopeArray.includes(e.target.value)) {
@@ -165,7 +173,6 @@ export class TaskSettingsComponent implements OnInit {
 
     //         }
 
-
     //       });
     //     })
     //   })
@@ -174,29 +181,9 @@ export class TaskSettingsComponent implements OnInit {
     // console.log(this.userPushTokens)
   }
 
-
-
-
-
-
   public deleteTask() {
-
-     this.taskService.deleteTask(this.taskData.taskId).then(() => {
-       this.router.navigate(['/dashboard'])
-     });
-
-
-    }
-
-
+    this.taskService.deleteTask(this.taskData.taskId).then(() => {
+      this.router.navigate(['/dashboard']);
+    });
   }
-
-
-
-
-
-
-
-
-
-
+}
